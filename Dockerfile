3edefa2374cfd75f852920dfe05c587b2961e255
AuroraSmil <54847918+AuroraSmil@users.noreply.github.com>
2025-03-03 13:48:13 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM cgr.dev/chainguard/jre:latest
+
+ENV LANG='nb_NO.UTF-8' LANGUAGE='nb_NO:nb' LC_ALL='nb:NO.UTF-8' TZ="Europe/Oslo"
+
+COPY build/libs/mediator-all.jar /app.jar
+
+ENTRYPOINT ["java", "-jar", "/app.jar"]
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "dp-SERVICENAME"
+dependencyResolutionManagement {
+    repositories {
+        maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
+    versionCatalogs {
+        create("libs") {
+            from("no.nav.dagpenger:dp-version-catalog:20250303.140.82ddc5")
+        }
+    }
+}
